37266cd7d46edae66985d5fd6fd20ae2eda882b9
PoMingChen <s410373092@gm.ntpu.edu.tw>
2024-07-25 17:44:02 +0800

    Scaffold: Build the redirection and the dynamic routing to certain restaurant with its id

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
+  // res.send('Hello the initiation of restaurant list!')
+  res.redirect('/restaurant')
+})
+
+app.get('/restaurant', (req, res) => {
   res.send('Hello the initiation of restaurant list!')
 })
+
+// The route '/restaurant/:id' captures the id parameter from the URL
+app.get('/restaurant/:id', (req, res) => {
+
+  // The req.params.id is used to access the id parameter within the route handler.
+  id = req.params.id
+  res.send(`The detail of restaurant ${id}`)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
